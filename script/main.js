7654424f2d0099419c10d7ee76dc927f15c6146b
Your NameIgor-Kay <kaygorodtsevie@gmail.com>
2024-01-30 17:39:32 +0700

    clean

--- script/main.js
+++ script/main.js
@@ -39,7 +39,7 @@ var light2 = new THREE.PointLight(0xFFFFFF, 0,5, 0);
 light2.position.set(0, 0, 2.677);
 scene.add(light2);
 
-const modelPath = 'assets/models/scene (11).gltf';
+const modelPath = 'assets/models/bot-drafts.gltf';
 
 const loader = new GLTFLoader();
 
